71ff991cf9e36467fe0bd525cf9011b6eda804da
Alireza <Alireza_paridar@hotmail.com>
2017-08-05 13:00:30 +0430

    Add project files.

---
+++ PerstianDateConvertor/Convertor.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Globalization;
+
+namespace PerstianDateConvertor
+{
+    public class Convertor
+    {
+
+        public static DateTime ToGregorianDate(PersianDate date)
+        {
+            PersianCalendar p = new PersianCalendar();
+            return p.ToDateTime(date.Year, date.Month, date.Day, date.Hour, date.Minute, date.Second, (int)date.Millisecond);
+        }
+
+        public static PersianDate ToPersianDate(DateTime date)
+        {
+            PersianCalendar p = new PersianCalendar();
+            return new PersianDate
+            {
+                Day = p.GetDayOfMonth(date),
+                Month = p.GetMonth(date),
+                Year = p.GetYear(date),
+
+                Hour = p.GetHour(date),
+                Minute = p.GetMinute(date),
+                Second = p.GetSecond(date),
+                Millisecond = p.GetMilliseconds(date)
+            };
+        }
+
+        public static DayOfWeek DayOfWeekend(PersianDate date)
+        {
+            PersianCalendar p = new PersianCalendar();
+            var temp = ToGregorianDate(date);
+            return p.GetDayOfWeek(temp);
+        }
+
+        public static string DayOfWeekString(PersianDate date)
+        {
+
+            DayOfWeek temp = DayOfWeekend(date);
+
+            switch (temp)
+            {
+                case DayOfWeek.Saturday: return "شنبه";
+                case DayOfWeek.Sunday: return "یکشنبه";
+                case DayOfWeek.Monday: return "دوشنبه";
+                case DayOfWeek.Tuesday: return "سه شنبه";
+                case DayOfWeek.Wednesday: return "چهارشنبه";
+                case DayOfWeek.Thursday: return "پنجشنبه";
+                case DayOfWeek.Friday: return "جمعه";
+            }
+
+            return "";
+        }
+    }
+}
---
+++ PerstianDateConvertor/PersianDate.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace PerstianDateConvertor
+{
+    public class PersianDate
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int Day { get; set; }
+
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+        public int Second { get; set; }
+        public double Millisecond { get; set; }
+
+
+        public string DayOfWeekString
+        {
+            get
+            {
+                return Convertor.DayOfWeekString(GetDate());
+            }
+        }
+
+        public DayOfWeek DayOfWeek
+        {
+            get
+            {
+                return Convertor.DayOfWeekend(GetDate());
+            }
+        }
+
+        public PersianDate Now
+        {
+            get
+            {
+                var temp = DateTime.Now;
+                return Convertor.ToPersianDate(temp);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Year}/{Month}/{Day} {Hour}:{Minute}:{Second}";
+        }
+
+        private PersianDate GetDate()
+        {
+            return new PersianDate
+            {
+                Year = Year,
+                Month = Month,
+                Day = Day,
+
+                Hour = Hour,
+                Minute = Minute,
+                Second = Second,
+                Millisecond = Millisecond
+            };
+        }
+
+    }
+}
